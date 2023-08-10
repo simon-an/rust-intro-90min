52f8534e4bae4f74777e3399e2a80bb6216352f0
simon-an <26556185+simon-an@users.noreply.github.com>
2023-08-10 22:35:24 +0000

    Deploying to gh-pages from @ simon-an/rust-intro-90min@3db4751ca31f5eb7299f1665838718ce57db9257 🚀

--- main.rs
+++ main.rs
@@ -1,66 +1,19 @@
 fn main() {
-    pub struct Animal {
-        pub name: String,
-        legs: u32,
-        pub(crate) wings: u32,
+    let x = parse("5");
+    println!("x is {}", x);
+
+    let res = try_parse("5");
+    match res {
+        Ok(x) => println!("x is {}", x),
+        Err(e) => println!("Error: {}", e),
     }
-    pub struct Dog {
-        pub name: String,
-        legs: u32,
-    }
-    pub enum AnimalTypes {
-        Chicken(Animal, u32),
-        Dog(Dog),
-        Other(String),
-        BadTypesystemUse((String, String, u32, String, String)),
-    }
+}
 
-    let list = vec![
-        AnimalTypes::Chicken(
-            Animal {
-                name: "Chicken".to_string(),
-                legs: 2,
-                wings: 2,
-            },
-            5,
-        ),
-        AnimalTypes::Chicken(
-            Animal {
-                name: "Chicken".to_string(),
-                legs: 2,
-                wings: 2,
-            },
-            3,
-        ),
-        AnimalTypes::Dog(Dog {
-            name: "Dog".to_string(),
-            legs: 4,
-        }),
-        AnimalTypes::Other("Other".to_string()),
-    ];
-    let res = list.iter().fold(0u32,
-        |acc, x| match x {
-            AnimalTypes::Chicken(.., count) => *count + acc,
-            AnimalTypes::Dog(_) => acc,
-            AnimalTypes::Other(_) => acc,
-            AnimalTypes::BadTypesystemUse(_) => {
-                panic!("Bad typesystem use")
-            }
-        }
-    );
-
-    println!("res: {}", res);
-
-    let legs = list.iter().fold(0u32,
-        |acc, x| match x {
-            AnimalTypes::Chicken(Animal{ legs, ..}, ..) => *legs + acc,
-            AnimalTypes::Dog(Dog { legs, ..}) => *legs + acc,
-            AnimalTypes::Other(_) => acc,
-            AnimalTypes::BadTypesystemUse(_) => {
-                panic!("Bad typesystem use")
-            }
-        }
-    );
-
-    println!("legs: {}", legs);
+fn parse(thing: &str) -> u32 {
+    return thing.parse().expect("Not a number");
+}
+fn try_parse(thing: &str) -> Result<u32, &str> {
+    // 'turbofish': ::<>
+    let val: Result<u32, std::num::ParseIntError> = thing.parse::<u32>();
+    val.map_err(|_e| "Not a number")
 }
